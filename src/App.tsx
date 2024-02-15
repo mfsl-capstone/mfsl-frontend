985ffbd2d5513028f3f92faff38a76bde81f30e2
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2024-02-15 15:00:16 -0500

    fix : CAP-55 - Removed the generated code by React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import './App.css';
 import {Outlet} from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 
 function App() {
   return (
 
-    <div className="App">
+    <div >
       <header className="App-header">
+          <NavBar/>
           <Outlet/>
-        {/*<p>*/}
-        {/*  Edit <code>src/App.tsx</code> and save to reload.*/}
-        {/*</p>*/}
-        {/*<a*/}
-        {/*  className="App-link"*/}
-        {/*  href="https://reactjs.org"*/}
-        {/*  target="_blank"*/}
-        {/*  rel="noopener noreferrer"*/}
-        {/*>*/}
-        {/*  Learn React*/}
-        {/*</a>*/}
       </header>
     </div>
   );
