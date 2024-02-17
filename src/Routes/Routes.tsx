a145e81e2cae709d944c9a61abe0bc068f375de4
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2024-02-17 17:22:01 -0500

    feat : CAP-39 - code that creates the ResultsPage

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -4,6 +4,7 @@ import App from "../App";
 import LoginPage from "../Pages/LoginPage/LoginPage";
 import SignUpPage from "../Pages/SignUpPage/SignUpPage";
 import StandingsPage from "../Pages/StandingsPage/StangingsPage";
+import ResultsPage from "../Pages/ResultsPage/ResultsPage";
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +14,7 @@ export const router = createBrowserRouter([
             {path: "", element: <LoginPage/>},
             {path: "signup", element: <SignUpPage/>},
             {path: "standings", element: <StandingsPage/>},
+            {path: "results", element: <ResultsPage/>},
 
         ]
     }
