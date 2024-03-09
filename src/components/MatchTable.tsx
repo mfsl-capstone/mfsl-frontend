16a3326d7ed6d6a3dd331507f8567d4493ec2cf2
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2024-03-08 20:55:57 -0500

    fix: CAP-40 - display issue for results in matchtable

--- src/components/MatchTable.tsx
+++ src/components/MatchTable.tsx
@@ -3,60 +3,55 @@ import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from
 import Typography from "@mui/material/Typography";
 
 interface MatchTableProps {
-  gameWeek: number;
-  matches: {
-    homeTeam: string;
-    awayTeam: string;
-    homeScore?: number;
-    awayScore?: number;
-  }[];
-  showScore?: boolean;
-  currentTeam?: string;
-  selectedTeam?: string; // Add selectedTeam attribute
+    gameWeek: number;
+    matches: {
+        homeTeam: string;
+        awayTeam: string;
+        homeScore?: number;
+        awayScore?: number;
+    }[];
+    showScore?: boolean;
+    currentTeam?: string;
+    selectedTeam?: string; // Add selectedTeam attribute
 }
 
 const MatchTable: React.FC<MatchTableProps> = ({ gameWeek, matches, showScore = true, currentTeam, selectedTeam }) => {
-  // Filter matches based on selectedTeam
-  const filteredMatches = selectedTeam ? matches.filter(match => match.homeTeam === selectedTeam || match.awayTeam === selectedTeam) : matches;
+    // Filter matches based on selectedTeam
+    const filteredMatches = selectedTeam ? matches.filter(match => match.homeTeam === selectedTeam || match.awayTeam === selectedTeam) : matches;
 
-  return (
-      <div style={{ marginBottom: '20px' }}>
-        <Typography variant="h6" sx={{ textAlign: 'left', marginLeft: '10px', marginBottom: '10px', color: '#e01a4f' }}>Game Week {gameWeek}</Typography>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell align="center" style={{ color: '#fff' }}>Home Team</TableCell>
-                {showScore && <TableCell align="center" style={{ color: '#fff' }}>Score</TableCell>}
-                {!showScore && <TableCell align="center" style={{ color: '#fff'}}></TableCell>}
-                <TableCell align="center" style={{ color: '#fff' }}>Away Team</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {filteredMatches.map((match, index) => (
-                  <TableRow key={index} sx={{
-                    backgroundColor: match.homeTeam === currentTeam || match.awayTeam === currentTeam ? '#e01a4f' : 'inherit',
-                    '&:hover': {
-                      backgroundColor: '#e01a4f',
-                    },
-                    '&:last-child td, &:last-child th': { border: 0 },
-                  }}>
-                    <TableCell align="center" style={{ color: '#fff' }}>{match.homeTeam}</TableCell>
-                    {showScore ? (match.homeScore && match.awayScore ? (
-                        <TableCell align="center" style={{ color: '#fff' }}>{match.homeScore} - {match.awayScore}</TableCell>
-                    ) : (
-                        <TableCell align="center" style={{ color: '#fff' }}>v</TableCell>
-                    )) : (
-                        <TableCell align="center" style={{ color: '#fff' }}>v</TableCell>
-                    )}
-                    <TableCell align="center" style={{ color: '#fff' }}>{match.awayTeam}</TableCell>
-                  </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
-  );
+    return (
+        <div style={{ marginBottom: '20px' }}>
+            <Typography variant="h6" sx={{ textAlign: 'left', marginLeft: '10px', marginBottom: '10px', color: '#e01a4f' }}>Game Week {gameWeek}</Typography>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center" style={{ color: '#fff' }}>Home Team</TableCell>
+                            {showScore && <TableCell align="center" style={{ color: '#fff' }}>Score</TableCell>}
+                            {!showScore && <TableCell align="center" style={{ color: '#fff' }}></TableCell>}
+                            <TableCell align="center" style={{ color: '#fff' }}>Away Team</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {filteredMatches.map((match, index) => (
+                            <TableRow key={index} sx={{
+                                backgroundColor: match.homeTeam === currentTeam || match.awayTeam === currentTeam ? '#e01a4f' : 'inherit',
+                                '&:hover': {
+                                    backgroundColor: '#e01a4f',
+                                },
+                                '&:last-child td, &:last-child th': { border: 0 },
+                            }}>
+                                <TableCell align="center" style={{ color: '#fff' }}>{match.homeTeam}</TableCell>
+                                {showScore && <TableCell align="center" style={{ color: '#fff' }}>{match.homeScore} - {match.awayScore}</TableCell>}
+                                {!showScore && <TableCell align="center" style={{ color: '#fff' }}>v</TableCell>}
+                                <TableCell align="center" style={{ color: '#fff' }}>{match.awayTeam}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    );
 }
 
 export default MatchTable;
