9a22becf1462c66e8cf95bd0260b7bb919c9c74b
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2024-03-16 17:36:28 -0400

    feat: CAP-65 - handled the login form to the backend

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { TextField, Button, Typography } from '@mui/material';
 import './LoginPage.css';
+import axios from 'axios';
 
 const LoginPage: React.FC = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState<string | null>(null)
+    
 
-    const handleLogin = () => {
-        console.log('Logging in with:', { username, password });
+    const handleLogin = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/user/login`, {
+                params: {
+                    username,
+                    password
+                }
+            });
+            console.log('Login successful:', response.data);
+        } catch (error) {
+            console.error('Login failed');
+            setError('Invalid username or password');
+        }
     };
+
+
     const isFormFilled = () => {
         return username.trim() !== '' && password.trim() !== '';
     };
@@ -36,6 +52,7 @@ const LoginPage: React.FC = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 className="login-input"
             />
+            {error && <Typography variant="body2" color="error" gutterBottom>{error}</Typography>}
             <Button variant="contained" className="login-button" onClick={handleLogin} disabled={!isFormFilled()}>
                 Login
             </Button>
