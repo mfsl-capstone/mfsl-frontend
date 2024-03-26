4f360ba2bd523ceefdd1ad9bef6649aae1560c95
abhijeetpraveen <abhijeetpraveen@gmail.com>
2024-03-26 03:49:43 -0400

    feat: CAP-36 - getting rid of navbar changes to prevent potential future merge conflict

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -62,14 +62,9 @@ function NavBar() {
                         ))}
                         {
                             isAuthenticated ? (
-                                <>
-                                    <Button component={Link} to={'/team-selection'} sx={{ color: '#fff', mx: 1 }}>
-                                        Pick Team
-                                    </Button>
-                                    <Button onClick={logout} sx={{ backgroundColor: '#e01a4f', color: '#fff' }}>
-                                        Sign Out
-                                    </Button>
-                                </>
+                                <Button onClick={logout} sx={{ backgroundColor: '#e01a4f', color: '#fff' }}>
+                                    Sign Out
+                                </Button>
                             ):(
                                 <Button component={Link} to="/signup" sx={{ backgroundColor: '#e01a4f', color: '#fff' }}>
                                     Sign Up
