159a312d83af4706f118aef772e89f778f449246
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2024-04-01 17:07:21 -0400

    feat: CAP-69 - finalize the matches page

--- src/Pages/RealMatchesPage/RealMatchesPage.tsx
+++ src/Pages/RealMatchesPage/RealMatchesPage.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Typography, Box, TextField, Grid } from '@mui/material';
-import { ToastContainer, toast } from "react-toastify";
+import React, {useEffect, useState} from 'react';
+import {Typography, Box, TextField, Grid, CircularProgress} from '@mui/material';
+import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { getGameStats } from "../../api/game";
+import {getGameStats} from "../../api/game";
 import MatchesCard from "./../../components/MatchesCard";
 
 interface GameData {
@@ -27,6 +27,7 @@ function RealMatchesPage() {
     const token = localStorage.getItem('token');
     const [selectedDate, setSelectedDate] = useState<string>(new Date().toISOString().slice(0, 10)); // Initial date is today
     const [games, setGames] = useState<GameData[]>([]);
+    const [loading, setLoading] = useState(true);
 
     const handleDateChange = (date: string) => {
         setSelectedDate(date);
@@ -34,9 +35,19 @@ function RealMatchesPage() {
 
     const getGames = async () => {
         try {
-            const response = await getGameStats(selectedDate, selectedDate, token);
-            const gamesData = response.data; // Extract data from the Axios response
+            setLoading(true);
+            const response = await getGameStats(selectedDate, selectedDate, token);//to check if it works
+            const gamesData = response.data.map((game: GameData) => {
+                const utcDate = new Date(Date.UTC(game.date[0], game.date[1] - 1, game.date[2], game.date[3], game.date[4]));
+                const localDate = new Date(utcDate.toLocaleString('en-US', { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone }));
+                return { ...game, localDate };
+            });
             setGames(gamesData);
+            setLoading(false);
+            if (gamesData.length === 0) {
+                showError("No game on chosen date.");
+                console.log("no game");
+            }
         } catch (error: any) {
             showError(error);
         }
@@ -71,8 +82,9 @@ function RealMatchesPage() {
 
     return (
         <div className="fixture-page-container">
-            <Typography variant="h2" sx={{ textAlign: 'left', marginLeft: '10px', color: '#e01a4f' }}>Matches</Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px', marginBottom: '20px' }}>
+            <Typography variant="h2"
+                        sx={{textAlign: 'left', marginLeft: '10px', color: '#e01a4f'}}>Matches</Typography>
+            <Box sx={{display: 'flex', alignItems: 'center', marginTop: '10px', marginBottom: '20px'}}>
                 <TextField
                     id="date"
                     label="Select Date"
@@ -80,22 +92,31 @@ function RealMatchesPage() {
                     type="date"
                     value={selectedDate}
                     onChange={(e) => handleDateChange(e.target.value)}
-                    sx={{ marginLeft: '30px', backgroundColor:'#fff' }}
+                    sx={{marginLeft: '30px', backgroundColor: '#fff'}}
                 />
             </Box>
-            <Grid container spacing={2}>
-                {games.map((game) => (
-                    <Grid item xs={12} sm={6} md={3} key={game.id}>
-                        <MatchesCard game={game} isCompleted={isGameCompleted(game)} />
+            {loading ? (
+                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+                    <CircularProgress sx={{color: "#ff0000"}}/>
+                </div>
+            ) : (
+                <>
+
+                    <Grid container spacing={2}>
+                        {games.map((game) => (
+                            <Grid item xs={12} sm={6} md={3} key={game.id}>
+                                <MatchesCard game={game} isCompleted={isGameCompleted(game)}/>
+                            </Grid>
+                        ))}
                     </Grid>
-                ))}
-            </Grid>
-            <ToastContainer />
+
+                </>
+            )}
+            <ToastContainer/>
         </div>
     )
 }
 
 export default RealMatchesPage;
-
-//default to current date
-// try four cards per row
+//scores go next to the team
+// vs goes in the middle
--- src/components/MatchesCard.tsx
+++ src/components/MatchesCard.tsx
@@ -27,30 +27,55 @@ const GameCard: React.FC<GameCardProps> = ({ game, isCompleted }) => {
     return (
         <Card>
             <CardContent>
-                {!isCompleted && (
-                    <Typography variant="body1" sx={{ marginBottom: 1 }}>
-                        {`${gameDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`}
-                    </Typography>
-                )}
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <img src={game.homeTeam.url} alt={game.homeTeam.name} style={{ width: '30px', height: '30px', marginRight: '10px' }} />
-                    <Typography variant="h6" sx={{ marginBottom: 1 }}>{game.homeTeam.name}</Typography>
+                <Typography variant="body1" sx={{ marginBottom: 1 }}>
+                    {!isCompleted ? gameDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : 'Full Time'}
+                </Typography>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'start', flexDirection: 'row' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+                        <img src={game.homeTeam.url} alt={game.homeTeam.name} style={{ width: '30px', height: '30px', marginBottom: '5px' }} />
+                        <Typography variant="h6" sx={{ marginBottom: 1 }}>{game.homeTeam.name}</Typography>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'end' }}>
+                        {isCompleted && (
+                            <Typography variant="body1" sx={{ color: '#e01a4f', marginTop: 1 }}>
+                                {`${game.homeTeamScore}`}
+                            </Typography>
+                        )}
+                    </div>
                 </div>
-                <div style={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
-                    <img src={game.awayTeam.url} alt={game.awayTeam.name} style={{ width: '30px', height: '30px', marginRight: '10px' }} />
-                    <Typography variant="h6" sx={{ marginBottom: 1 }}>{game.awayTeam.name}</Typography>
+                <div style={{ display: 'flex', alignItems: 'start', flexDirection: 'column' }}>
+
+                        <Typography variant="body1" sx={{ marginTop: 1, marginBottom: 1 }}>vs</Typography>
+
                 </div>
-                {isCompleted && (
-                    <Typography variant="body1" sx={{ color: '#e01a4f', marginTop: '10px' }}>
-                        {`${game.homeTeamScore} - ${game.awayTeamScore}`}
-                    </Typography>
-                )}
-                {!isCompleted && (
-                    <Typography variant="body1" sx={{ marginTop: '10px' }}>vs</Typography>
-                )}
+
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'start', flexDirection: 'row' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+                        <img src={game.awayTeam.url} alt={game.awayTeam.name} style={{ width: '30px', height: '30px', marginBottom: '5px' }} />
+                        <Typography variant="h6" sx={{ marginBottom: 1 }}>{game.awayTeam.name}</Typography>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'end' }}>
+                        {isCompleted && (
+                            <Typography variant="body1" sx={{ color: '#e01a4f', marginTop: 1 }}>
+                                {`${game.awayTeamScore}`}
+                            </Typography>
+                        )}
+                    </div>
+                </div>
+
+
             </CardContent>
         </Card>
     );
 }
 
 export default GameCard;
+// <div style={{ display: 'flex', alignItems: isCompleted ? 'end' : 'start', flexDirection: 'column' }}>
+//     {!isCompleted ? (
+//         <Typography variant="body1" sx={{ marginTop: 1 }}>vs</Typography>
+//     ) : (
+//         <Typography variant="body1" sx={{ color: '#e01a4f', marginTop: 1 }}>
+//             {`${game.homeTeamScore} - ${game.awayTeamScore}`}
+//         </Typography>
+//     )}
+// </div>
