3b91603ef0a7fbc85be26ccea5c838f1bbeb10c6
rooshnie23 <90059929+rooshnie23@users.noreply.github.com>
2024-04-02 02:21:42 -0400

    feat: CAP-104 - connected to the backend

--- src/Pages/FixturePage/FixturePage.tsx
+++ src/Pages/FixturePage/FixturePage.tsx
@@ -4,62 +4,35 @@ import Box from "@mui/material/Box";
 import MatchTable from '../../components/MatchTable';
 import TeamPicker from "../../components/TeamPicker";
 import './FixturePage.css';
-import {getResultsLeague} from "../../api/league";
+import {getFixturesLeague} from "../../api/league";
 import {toast} from "react-toastify";
 
 
 function FixturePage() {
     const [currentUserTeam, setCurrentUserTeam] = useState<string>(''); // State to hold the current user's team name
-    const resultsData = [
-        {
-            gameWeek: 1,
-            matches: [
-                {homeTeam: 'Team 1', awayTeam: 'Team 2'},
-                {homeTeam: 'Team 2', awayTeam: 'Team 8',},
-                {homeTeam: 'Team 3', awayTeam: 'Team 9',},
-                {homeTeam: 'Team 4', awayTeam: 'Team 6',}
-
-            ]
-        },
-        {
-            gameWeek: 2,
-            matches: [
-                {homeTeam: 'Team 3', awayTeam: 'Team 4'},
-                {homeTeam: 'Team 1', awayTeam: 'Team 8',},
-                {homeTeam: 'Team 9', awayTeam: 'Team 5',},
-                {homeTeam: 'Team 7', awayTeam: 'Team 9',}
-            ]
-        },
-        {
-            gameWeek: 4,
-            matches: [
-                {homeTeam: 'Team 4', awayTeam: 'Team 2'},
-                {homeTeam: 'Team 5', awayTeam: 'Team 6',},
-                {homeTeam: 'Team 7', awayTeam: 'Team 8',},
-                {homeTeam: 'Team 1', awayTeam: 'Team 5',}
-
-            ]
-        },
-        {
-            gameWeek: 5,
-            matches: [
-                {homeTeam: 'Team 1', awayTeam: 'Team 6'},
-                {homeTeam: 'Team 2', awayTeam: 'Team 5',},
-                {homeTeam: 'Team 3', awayTeam: 'Team 3',},
-                {homeTeam: 'Team 4', awayTeam: 'Team 2',}
-            ]
-        },
-    ];
-
+    const [fixturesData, setFixturesData] = useState<any[]>([]);
     const [selectedTeam, setSelectedTeam] = useState<string>('');
-
-    const getResults = async () => {
+    const token = localStorage.getItem('token');
+    const leagueId = localStorage.getItem('chosenLeagueId');
+    const getFixtures = async () => {
         try {
+            const responseData = await getFixturesLeague(leagueId, token);
 
-        } catch (error: any) {
-            showError(error);
+            const transformedData = responseData.weeks.map((weekArray: any[]) => ({
+                gameWeek: weekArray[0].weekNumber,
+                matches: weekArray.map(match => ({
+                    homeTeam: match.fantasyTeamA,
+                    awayTeam: match.fantasyTeamB,
+                    homeScore: match.teamAScore,
+                    awayScore: match.teamBScore
+                }))
+            }));
+            setFixturesData(transformedData); // Update the resultsData state with transformed data
+        } catch (error) {
+            showError("Error fetching data. Please try again later.");
         }
     };
+
     const showError = (message: string): void => {
         toast.error(message, {
             position: "top-right",
@@ -82,9 +55,9 @@ function FixturePage() {
     }
 
     useEffect(() => {
-        setCurrentUserTeam("Team 1");
+        getFixtures();
+        setCurrentUserTeam("Team 1"); // Set default current user's team
     }, []);
-
     return (
         <div className="fixture-page-container">
             <Typography variant="h2" sx={{textAlign: 'left', marginLeft: '10px', color: '#e01a4f'}}>League
@@ -93,10 +66,10 @@ function FixturePage() {
                 <Typography variant="subtitle1"
                             sx={{textAlign: 'left', marginLeft: '30px', color: '#e01a4f', paddingRight: '10px'}}>Filter
                     Matches:</Typography>
-                <TeamPicker resultsData={resultsData} onTeamSelect={handleTeamSelect}/>
+                <TeamPicker resultsData={fixturesData} onTeamSelect={handleTeamSelect}/>
             </Box>
             <div className="fixture-table-container">
-                {resultsData.map((weekData, index) => (
+                {fixturesData.map((weekData, index) => (
                     <MatchTable key={index} gameWeek={weekData.gameWeek} matches={weekData.matches} showScore={false}
                                 currentTeam={currentUserTeam} selectedTeam={selectedTeam}/>
                 ))}
